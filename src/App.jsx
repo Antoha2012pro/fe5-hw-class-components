d9f53b25f9503bf4c3a5e494a6931d9bd88a60e9
Anttoha <167896136+Antoha2012pro@users.noreply.github.com>
2026-06-17 19:41:07 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import "./App.css";
+import TaskList from "./components/TaskList";
+
+const initialTasks = [
+  {
+    id: "1",
+    text: "qwe",
+  },
+  {
+    id: "2",
+    text: "asd",
+  },
+  {
+    id: "3",
+    text: "zxc",
+  },
+];
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: initialTasks,
+    };
+  }
+
+  deleteTask = (idToDelete) => {
+    this.setState({
+      tasks: this.state.tasks.filter((task) => task.id !== idToDelete),
+    });
+  };
+
+  addTask = (text) => {
+    const newTask = {
+      id: Date.now().toString(),
+      text: text,
+    };
+    this.setState({
+      tasks: [...this.state.tasks, newTask],
+    });
+  };
+
+  render() {
+    const { tasks } = this.state;
+
+    return (
+      <div className="my-0 mx-auto py-0 px-4 md:w-[768px] space-y-3">
+        <h2 className="text-center font-medium text-3xl">Список завдань</h2>
+        <TaskList
+          tasks={tasks}
+          onDelete={this.deleteTask}
+          onAdd={this.addTask}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+
+export default class TaskList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTaskValue: "",
+      isNewTaskHidden: true,
+    };
+  }
+
+  render() {
+    const { tasks, onDelete, onAdd } = this.props;
+    const { newTaskValue, isNewTaskHidden } = this.state;
+    return (
+      <div className="flex flex-col md:flex-row justify-between gap-10 md:gap-0">
+        <div className="flex flex-col w-full max-w-70 items-start">
+          <label className="font-mono text-1xl">Нова Задача</label>
+          <input
+            className="mb-3 border border-blue-200 py-1 px-2 rounded-xl focus:outline-none w-full"
+            type="text"
+            placeholder="Помити посуд..."
+            value={newTaskValue}
+            onChange={(event) => {
+              const value = event.target.value;
+              this.setState({
+                newTaskValue: value,
+                isNewTaskHidden: value.trim().length === 0,
+              });
+            }}
+          />
+
+          {isNewTaskHidden ? (
+            <p className="bg-gray-600 text-white py-1 px-2 rounded-xl cursor-not-allowed">
+              Напиши свою першу задачу!
+            </p>
+          ) : (
+            <button
+              className="bg-blue-500 border-none text-black rounded-2xl py-2 px-4 focus:outline-none hover:opacity-80 transition-all hover:cursor-pointer"
+              onClick={(event) => {
+                event.preventDefault();
+                onAdd(newTaskValue);
+                this.setState({ newTaskValue: "", isNewTaskHidden: true });
+              }}
+            >
+              Додати задачу
+            </button>
+          )}
+        </div>
+
+        <ul className="w-full max-w-80 flex flex-wrap gap-6">
+          {tasks.map(({ id, text }) => (
+            <li
+              className="group bg-blue-200 rounded-xl py-2 px-2 relative flex flex-col gap-2"
+              key={id}
+            >
+              <p>{text}</p>
+              <button
+                className="opacity-0 group-hover:opacity-100 transition-opacity bg-red-500 text-white rounded-xl px-2 py-1 hover:opacity-80 hover:cursor-pointer"
+                onClick={() => onDelete(id)}
+              >
+                Видалити
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
